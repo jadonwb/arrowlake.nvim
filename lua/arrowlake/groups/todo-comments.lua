8175abaa19aa364e26819622a759b4d0f496f449
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-01 16:55:40 -0500

    add highlights for todo comments

--- lua/arrowlake/groups/todo-comments.lua
+++ lua/arrowlake/groups/todo-comments.lua
@@ -8,7 +8,13 @@ M.url = "https://github.com/folke/todo-comments.nvim"
 function M.get(c, opts)
   -- stylua: ignore
   return {
-
+    -- TODO: move the todo comments plugin config into here instead (just the keywords and colors)
+    ArrowlakeCommentTodo = { fg = c.todo },
+    ArrowlakeCommentFixme = {fg = c.error },
+    ArrowlakeCommentWarn = { fg = c.warning },
+    ArrowlakeCommentNote = { fg = c.hint },
+    ArrowlakeCommentPerf = { fg = c.keyword }, -- TODO: make a comment class/table with first class perf
+    ArrowlakeCommentTest = { fg = c.parameter }, -- TODO: same
   }
 end
 
