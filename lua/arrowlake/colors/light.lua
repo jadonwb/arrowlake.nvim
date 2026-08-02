e2151a541553be7478a61c354ee5e2e3d3cdcace
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-01 21:40:33 -0500

    ok

--- lua/arrowlake/colors/light.lua
+++ lua/arrowlake/colors/light.lua
@@ -15,7 +15,7 @@ M.fg_gutter = "#ad9b88"
 -- Core colors
 M.red = "#BC495D"
 M.orange = "#CF9978"
-M.yellow = "#B5A24F"
+M.yellow = "#AE984C"
 M.green = "#4E7469"
 M.blue = "#4A74A8"
 M.purple = "#6660AF"
