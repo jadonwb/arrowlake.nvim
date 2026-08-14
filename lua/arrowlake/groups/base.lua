e945c5531ea8d3eafbcfa7f914a4c17ccba0e84b
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2026-08-14 14:08:23 -0500

    add bold to search

--- lua/arrowlake/groups/base.lua
+++ lua/arrowlake/groups/base.lua
@@ -58,8 +58,8 @@ function M.get(c, opts)
     PmenuThumb                  = { bg = c.foregrounds.muted }, -- Popup menu: Thumb of the scrollbar.
     Question                    = { fg = c.ui.prompt }, -- |hit-enter| prompt and yes/no questions
     QuickFixLine                = { bg = c.backgrounds.visual, bold = true }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-    Search                      = { bg = c.backgrounds.search, fg = c.foregrounds.normal }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
-    IncSearch                   = { bg = c.backgrounds.search_current, fg = c.foregrounds.normal }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+    Search                      = { bg = c.backgrounds.search, fg = c.foregrounds.normal, bold = true }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
+    IncSearch                   = { bg = c.backgrounds.search_current, fg = c.foregrounds.normal, bold = true }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
     CurSearch                   =  "IncSearch",
     SpecialKey                  = { fg = c.foregrounds.darker }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
     SpellBad                    = { sp = c.diagnostics.error, undercurl = true }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
